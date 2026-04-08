714650881a8185f90c625775bd88edfb28cbce47
omartinez <its.osmartinez@gmail.com>
2026-04-08 17:37:43 +0200

    change solved exercise 1

--- 01_cores/ejemplo_resuelto.c
+++ 01_cores/ejemplo_resuelto.c
@@ -6,8 +6,11 @@ int main(void) {
 
     #pragma omp parallel
     {
+        printf("Parallel ejecutado por hilo %d\n", omp_get_thread_num());
+
         #pragma omp single
         {
+            printf("Single ejecutado por hilo %d\n", omp_get_thread_num());
             printf("Hilos en la región paralela: %d\n", omp_get_num_threads());
         }
     }
